4a18d28242ab3508c671d2a797449af2cd7078c8
Blowerlop <72097300+Blowerlop@users.noreply.github.com>
2022-12-13 15:23:05 +0100

    Update Input Manager Pattern

--- JabJob/Assets/_Project/Scripts/Managers/InputManager.cs
+++ JabJob/Assets/_Project/Scripts/Managers/InputManager.cs
@@ -5,10 +5,25 @@ using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    public Vector2 move;
+    #region Singleton
+    public static InputManager instance { get; private set; }
 
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+
+    #region Variables
+    public Vector2 move;
+    #endregion
+
+
+    #region Methods
     public void OnMove(InputValue inputValue)
     {
         move = inputValue.Get<Vector2>();
     }
+    #endregion
 }
