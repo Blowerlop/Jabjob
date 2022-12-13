2ca7c7f993bf853716c1cb83ec27eb4f9a4e0dfb
Blowerlop <72097300+Blowerlop@users.noreply.github.com>
2022-12-13 14:59:24 +0100

    Add InputManager pattern

--- JabJob/Assets/_Project/Scripts/Managers/InputManager.cs
+++ JabJob/Assets/_Project/Scripts/Managers/InputManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector2 move;
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(InputValue inputValue)
     {
-        
+        move = inputValue.Get<Vector2>();
     }
 }
