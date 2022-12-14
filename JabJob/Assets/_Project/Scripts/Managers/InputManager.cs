da44be57974d5115fb20e8e1789597da716ef396
Drainor <60524904+Drainor-GameDev@users.noreply.github.com>
2022-12-14 10:45:27 +0100

    Merge With Controller

---
+++ JabJob/Assets/_Project/Scripts/Managers/InputManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    #region Singleton
+    public static InputManager instance { get; private set; }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+
+    #region Variables
+    public Vector2 move;
+    public Vector2 look;
+    public bool isJumping;
+    #endregion
+
+
+    #region Methods
+    public void OnMove(InputValue inputValue)
+    {
+        move = inputValue.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue inputValue)
+    {
+        look = inputValue.Get<Vector2>();
+        look.y *= -1;
+    }
+
+    public void OnJump()
+    {
+        isJumping = true;
+    }
+    #endregion
+}
---
+++ JabJob/Assets/_Project/Scripts/Player/Controllers/MovementController.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    #region Variables
+    [Header("Movements")]
+    [SerializeField] private float _moveSpeed = 6.0f;
+    [SerializeField] private float _jumpHeight = 2.0f;
+    [SerializeField] private float _gravityForce = -9.81f;
+    [SerializeField] private bool _gravityEnabled = true;
+    [SerializeField] private float _maximumVerticalVelocity = -40.0f;
+    [SerializeField] private float _groundedVerticalVelocity = -2.0f;
+    private bool _isGrounded;
+    private float _verticalVelocity;
+
+    [Header("References")]
+    private Rigidbody _rigidbody;
+    private CharacterController _characterController;
+    #endregion
+
+
+    #region Updates
+    private void Awake()
+    {
+        //_rigidbody = GetComponent<Rigidbody>();
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        CheckGrounded();
+        PerformJumpAndGravity();
+        PerfomMovement();
+    }
+    #endregion
+
+
+    #region Methods
+    private void CheckGrounded()
+    {
+        _isGrounded = _characterController.isGrounded;
+    }
+
+    private void PerformJumpAndGravity()
+    {
+        if (_gravityEnabled == false) return;
+
+        if (_isGrounded)
+        {
+            if (_verticalVelocity < 0.0f)
+            {
+                _verticalVelocity = _groundedVerticalVelocity;
+            }
+
+            if ( InputManager.instance.isJumping)
+            {
+                _verticalVelocity = Mathf.Sqrt(-2.0f * _gravityForce * _jumpHeight);
+            }
+        }
+        else
+        {
+            if (_verticalVelocity > _maximumVerticalVelocity)
+            {
+                _verticalVelocity += _gravityForce * Time.deltaTime;
+            }
+        }
+
+        InputManager.instance.isJumping = false;
+    }
+
+    private void PerfomMovement()
+    {
+        float targetVelocity;
+        if (InputManager.instance.move == Vector2.zero)
+        {
+            targetVelocity = 0.0f;
+        }
+        else
+        {
+            targetVelocity = _moveSpeed;
+        }
+
+        float currentVelocity = new Vector3(_characterController.velocity.x, 0.0f, _characterController.velocity.z).magnitude;
+        float velocityToApply;
+
+        if (currentVelocity < targetVelocity - 0.1f || currentVelocity > targetVelocity + 0.1f)
+        {
+            velocityToApply = Mathf.Lerp(currentVelocity, targetVelocity, Time.deltaTime * 10.0f);
+        }
+        else
+        {
+            velocityToApply = targetVelocity;
+        }
+
+
+
+
+
+        Vector2 moveHorizontalVelocity = InputManager.instance.move * velocityToApply;
+        Vector3 moveVerticalVelocity = Vector3.up * _verticalVelocity;
+
+
+        _characterController.Move(
+            (
+            ((moveHorizontalVelocity.x * transform.right + moveHorizontalVelocity.y * transform.forward) + moveVerticalVelocity)
+            * Time.deltaTime
+            ));
+
+        
+
+    }
+    #endregion
+}
