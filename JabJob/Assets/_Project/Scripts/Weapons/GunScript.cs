620e39b9865474ad65a48d705cbaf919183399a7
Blowerlop <72097300+Blowerlop@users.noreply.github.com>
2022-12-16 10:05:46 +0100

    Revert "Merge Weapon"
    This reverts commit 67478cba121a18755ad882444f15c6805f3815c3.

--- JabJob/Assets/_Project/Scripts/Weapons/GunScript.cs
+++ JabJob/Assets/_Project/Scripts/Weapons/GunScript.cs
@@ -48,13 +48,8 @@ public class GunScript : NetworkBehaviour
                 }
                 nextShoot = Time.time + shootRate;
                 ammo--;
-<<<<<<< HEAD
                 LocalShoot();
                 ShootServerRpc();
-=======
-                LocalShoot(gun.transform.position, gun.transform.rotation);
-                ShootServerRpc(gun.transform.position, gun.transform.rotation);
->>>>>>> Weapons-Guillaume
             }
         }
         if (!InputManager.instance.shoot)
@@ -64,38 +59,22 @@ public class GunScript : NetworkBehaviour
     }
 
     [ServerRpc]
-<<<<<<< HEAD
     private void ShootServerRpc()
     {
         ShootClientRpc();
-=======
-    private void ShootServerRpc(Vector3 position, Quaternion rotation)
-    {
-        ShootClientRpc(position, rotation);
->>>>>>> Weapons-Guillaume
     }
 
 
     [ClientRpc]
-<<<<<<< HEAD
     private void ShootClientRpc()
     {
         if (IsOwner == false) LocalShoot();
-=======
-    private void ShootClientRpc(Vector3 position, Quaternion rotation)
-    {
-        if (IsOwner == false) LocalShoot(position, rotation);
->>>>>>> Weapons-Guillaume
     }
 
     /// <summary>
     /// Shoot and instanciate the projectile in term of the actual weapon 
     /// </summary>
-<<<<<<< HEAD
     public void LocalShoot()
-=======
-    public void LocalShoot(Vector3 position, Quaternion rotation)
->>>>>>> Weapons-Guillaume
     {
         RaycastHit hit;
         Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
@@ -106,26 +85,16 @@ public class GunScript : NetworkBehaviour
                 for (int i = 0; i < actualWeapon.bulletNumber; i++)
                 {
                     GameObject go = ObjectPoolingManager.instance.GetObject();
-<<<<<<< HEAD
                     go.transform.position = gun.transform.position;
                     go.transform.rotation = Quaternion.identity;
-=======
-                    go.transform.position = position;
-                    go.transform.rotation = rotation;
->>>>>>> Weapons-Guillaume
                     go.GetComponent<ProjectileScript>().Init(hit.point, actualWeapon.dispertion);
                 }
             }
             else
             {
                 GameObject go = ObjectPoolingManager.instance.GetObject();
-<<<<<<< HEAD
                 go.transform.position = gun.transform.position;
                 go.transform.rotation = Quaternion.identity;
-=======
-                go.transform.position = position;
-                go.transform.rotation = rotation;
->>>>>>> Weapons-Guillaume
                 go.GetComponent<ProjectileScript>().Init(hit.point, actualWeapon.dispertion);
             }
         }
@@ -136,26 +105,16 @@ public class GunScript : NetworkBehaviour
                 for (int i = 0; i < actualWeapon.bulletNumber; i++)
                 {
                     GameObject go = ObjectPoolingManager.instance.GetObject();
-<<<<<<< HEAD
                     go.transform.position = gun.transform.position;
                     go.transform.rotation = gun.transform.rotation;
-=======
-                    go.transform.position = position;
-                    go.transform.rotation = rotation;
->>>>>>> Weapons-Guillaume
                     go.GetComponent<ProjectileScript>().Init(actualWeapon.dispertion);
                 }
             }
             else
             {
                 GameObject go = ObjectPoolingManager.instance.GetObject();
-<<<<<<< HEAD
                 go.transform.position = gun.transform.position;
                 go.transform.rotation = gun.transform.rotation;
-=======
-                go.transform.position = position;
-                go.transform.rotation = rotation;
->>>>>>> Weapons-Guillaume
                 go.GetComponent<ProjectileScript>().Init(actualWeapon.dispertion);
             }
         }
