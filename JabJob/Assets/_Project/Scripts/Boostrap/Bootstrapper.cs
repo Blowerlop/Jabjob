33c9b70a36bd7c0d57cddf13487bb0d46d83b509
Blowerlop <72097300+Blowerlop@users.noreply.github.com>
2022-12-16 10:25:05 +0100

    Reset Main

--- JabJob/Assets/_Project/Scripts/Boostrap/Bootstrapper.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Bootstrapper : MonoBehaviour
-{
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)] 
-    public static void Execute() => Object.DontDestroyOnLoad(Object.Instantiate(Resources.Load("Systems")));
-}
