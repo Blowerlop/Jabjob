74b4a7d4518ed09c0aba6ab29066fafc9365ff9d
Blowerlop <chrisazur@gmx.fr>
2023-01-18 00:21:54 +0100

    [FINAL PUSh]  Polish System & Add Sample Scene

---
+++ Assets/_Project/Scripts/Event/Event Samples Scripts/EventSampleListener.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Project
+{
+    public class EventSampleListener : NetworkBehaviour, IGameEventListener
+    {
+        public void OnEnable()
+        {
+            GameEvent.onGlobalEventSample.Subscribe(SpawnObjectLocal);
+            //GameEvent.onGlobal.Subscribe(() => {SpawnObjectServerRpc();});
+            GameEvent.onGlobalEventSample.Subscribe(SpawnObjectServerRpcV2);
+            
+            GameEvent.onLocalEventSample.Subscribe(SpawnObjectLocal);
+        }
+
+        public void OnDisable()
+        {
+            GameEvent.onGlobalEventSample.Unsubscribe(SpawnObjectLocal);
+            //GameEvent.onGlobal.Unsubscribe(() => {SpawnObjectServerRpc();});
+            GameEvent.onGlobalEventSample.Unsubscribe(SpawnObjectServerRpcV2);
+            
+            GameEvent.onLocalEventSample.Unsubscribe(SpawnObjectLocal);
+        }
+
+
+
+        private void SpawnObjectServerRpcV2()
+        {
+            SpawnObjectServerRpc();
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        private void SpawnObjectServerRpc(ServerRpcParams serverRpcParams = default)
+        {
+            Debug.Log("Server RPC");
+            
+            List<ulong> nonLocalClientsIds = NetworkUtilities.GetAllNonLocalConnectedClientsIds(serverRpcParams);
+            SpawnObjectClientRpc(NetworkUtilities.GetNewClientRpcSenderParams(nonLocalClientsIds));
+        }
+        
+        [ClientRpc]
+        private void SpawnObjectClientRpc(ClientRpcParams clientRpcParams)
+        {
+            Debug.Log("Client RPC");
+            SpawnObjectLocal();      
+        }
+        
+        private void SpawnObjectLocal()
+        {
+            Debug.Log("Local RPC");
+            var go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            go.transform.position = new Vector3(0,0, (int)NetworkManager.Singleton.LocalClientId);
+        }
+        
+    }
+}
--- Assets/_Project/Scripts/Event/GameEvent.cs
+++ Assets/_Project/Scripts/Event/GameEvent.cs
@@ -2,6 +2,13 @@ namespace Project
 {
     public static class GameEvent
     {
-        //public static readonly Event onPlayerDeath = new Event();
+        #region  Samples
+        
+        // Samples
+        public static readonly Event onGlobalEventSample = new Event();
+        public static readonly Event onLocalEventSample = new Event();
+        
+        #endregion
+        
     }
 }
