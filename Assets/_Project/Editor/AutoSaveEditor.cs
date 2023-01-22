8f225ebb80dd3fba2bab486472e4d617d0706b32
Blowerlop <chrisazur@gmx.fr>
2023-01-22 16:44:41 +0100

    Added editing tools - 2 auto save scripts
    First Script that come from Tarodev:
    - Saving the editor at each X time interval
    Second Script:
    - Saving the editor each time we entering in the editor

---
+++ Assets/_Project/Editor/AutoSaveEditor.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Project
+{
+    
+    public static class AutoSaveEditor
+    {
+        [InitializeOnLoadMethod]
+        private static void Subscribe()
+        {
+            EditorApplication.playModeStateChanged += OnEnteredEditMode;
+            EditorApplication.quitting += OnEditorQuitting;
+        }
+
+        private static void OnEnteredEditMode(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.EnteredEditMode)
+            {
+                EditorSceneManager.SaveOpenScenes();
+                AssetDatabase.SaveAssets();
+                Debug.Log($"Auto-saved on EnteredEditMode at {DateTime.Now:h:mm:ss tt}");
+            }
+        }
+
+        private static void OnEditorQuitting()
+        {
+            EditorApplication.playModeStateChanged -= OnEnteredEditMode;
+        }
+    }
+}
