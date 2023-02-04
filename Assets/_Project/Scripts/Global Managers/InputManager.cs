acf7a1129db872519f26f520d35dc574dcd2c308
Blowerlop <chrisazur@gmx.fr>
2023-02-04 16:03:24 +0100

    Revert "Revert "Merge pull request #11 from Blowerlop/Console""
    This reverts commit a95a587dc50acb9a71aa3a4a48ede2bc4e265852.

--- Assets/_Project/Scripts/Global Managers/InputManager.cs
+++ Assets/_Project/Scripts/Global Managers/InputManager.cs
@@ -23,8 +23,9 @@ public class InputManager : MonoBehaviour
     public Vector2 move;
     public Vector2 look;
     public bool isJumping, shoot;
-    public UnityEvent reload;
+    public UnityEvent reload, openCommand;
     public bool isDashing;
+    public bool isConsoleOpened;
     #endregion
 
 
@@ -62,6 +63,12 @@ public class InputManager : MonoBehaviour
     public void OnDash()
     {
         isDashing = true;
+    }
+
+    public void OnConsole()
+    {
+        isConsoleOpened = !isConsoleOpened;
+        openCommand.Invoke();
     }
     #endregion
 }
