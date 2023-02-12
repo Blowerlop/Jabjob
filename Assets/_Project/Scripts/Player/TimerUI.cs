38bbae7beea7a347fe5d09ec804bd4ab02844bc1
Bao Nguyen <qbnguyen@outlook.fr>
2023-02-12 12:01:49 +0100

    Try map

--- Assets/_Project/Scripts/Player/TimerUI.cs
+++ Assets/_Project/Scripts/Player/TimerUI.cs
@@ -16,21 +16,21 @@ public class TimerUI : MonoBehaviour
         timerText = UI.FindComponentInChildWithTag<TextMeshProUGUI>("Timer");
     }
 
-    private void Update()
-    {
-        float t = NetworkTimer.instance.GetElapsedTime();
+    //private void Update()
+    //{
+    //    float t = NetworkTimer.instance.GetElapsedTime();
 
 
-        string minutes = Mathf.Abs(((int)t / 60)).ToString();
+    //    string minutes = Mathf.Abs(((int)t / 60)).ToString();
 
-        string seconds;
-        //Permet un affichage 0 : 00 et non pas 0 : 5 si les secondes sont inferieur � 10
-        if (Mathf.Abs(t % 60) <= 9.5) { seconds = "0" + Mathf.Abs((t % 60)).ToString("f0"); }
-        else if (Mathf.Abs(t % 60) >= 59.3) { seconds = "59"; }
-        else { seconds = Mathf.Abs((t % 60)).ToString("f0"); }
+    //    string seconds;
+    //    //Permet un affichage 0 : 00 et non pas 0 : 5 si les secondes sont inferieur � 10
+    //    if (Mathf.Abs(t % 60) <= 9.5) { seconds = "0" + Mathf.Abs((t % 60)).ToString("f0"); }
+    //    else if (Mathf.Abs(t % 60) >= 59.3) { seconds = "59"; }
+    //    else { seconds = Mathf.Abs((t % 60)).ToString("f0"); }
 
-        timerText.text = minutes + ":" + seconds;
-    }
+    //    timerText.text = minutes + ":" + seconds;
+    //}
 }
 
 public static class Helper
