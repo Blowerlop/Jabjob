b410327d9ea5d5625481a8962b0df7949049e6d0
EwenR <ewenriallan@gmail.com>
2023-03-24 16:39:12 +0100

    Working player animation system
    Next step : the implementation of the system in the multiplayer controller

--- Assets/_Project/Scripts/Animation/PlayerAnimation.cs
+++ Assets/_Project/Scripts/Animation/PlayerAnimation.cs
@@ -6,20 +6,40 @@ namespace Project
 {
     public class PlayerAnimation : MonoBehaviour
     {
-        private Animator mouvementAnimator;
-
-        private int x = 0, y = 0;
+        private float speed = 8f;
+        private Animator movementAnimator;
 
         // Start is called before the first frame update
         void Awake()
         {
-            mouvementAnimator = GetComponentInChildren<Animator>();
+            movementAnimator = GetComponentInChildren<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
 
+            Vector3 movemet = new Vector3(horizontal, 0f, vertical);
+
+            if (movemet.magnitude > 0f)
+            {
+                movemet.Normalize();
+                movemet *= speed * Time.deltaTime;
+            }
+
+            float velocityZ = Vector3.Dot(movemet.normalized, transform.forward);
+            float velocityX = Vector3.Dot(movemet.normalized, transform.right);
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                velocityZ *= 2;
+                velocityX *= 2;
+            }
+
+            movementAnimator.SetFloat("VelocityZ", velocityZ, 0.1f, Time.deltaTime);
+            movementAnimator.SetFloat("VelocityX", velocityX, 0.1f, Time.deltaTime);
         }
     }
 }
