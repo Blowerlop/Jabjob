fc7977c201c0039a4f5a366b345ee8da64651a68
Blowerlop <Blowerlop@users.noreply.github.com>
2023-04-15 20:31:34 +0200

    Added new player UI behaviour
    Ui is added in the Player prefab but destroyed in if the player in the network is not owner

---
+++ Assets/_Project/Scripts/UI/PlayerSetup.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Project
+{
+    public class PlayerSetup : NetworkBehaviour
+    {
+        [SerializeField] private Behaviour[] _componentsToDisableIfNotTheOwner;
+        [SerializeField] private GameObject[] _gameObjectToDestroyIfNotTheOwner;
+
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner) return;
+
+            
+            foreach (var componentToDestroy in _componentsToDisableIfNotTheOwner)
+            {
+                componentToDestroy.enabled = false;
+            }
+
+            foreach (var gameObjectToDestroy in _gameObjectToDestroyIfNotTheOwner)
+            {
+                Destroy(gameObjectToDestroy);
+            }
+        }
+    }
+}
